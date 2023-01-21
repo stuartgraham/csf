547b069fc1cfe1a939178c8a7aa404f648518ea2
Stuart Graham <stuart@stuart-graham.com>
2023-01-21 22:46:12 +0000

    Debug

--- html/js/modal-data.js
+++ html/js/modal-data.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var API_URL = "https://fd25hjufdwhtrnbmnuglrsvigq0ayuef.lambda-url.eu-west-1.on.aws/"
     $.getJSON(API_URL, function(data) {
       // Update div with ID "data-container"
+      console.log(data);
       $("#sample-data").html(data['ctRoot'][0]['description']);
     });
   });
