159e59f71c6647b34a601abe3bcc9462fca6e52d
Stuart Graham <stuart@stuart-graham.com>
2023-01-21 23:56:05 +0000

    Debug

--- html/js/modal-data.js
+++ html/js/modal-data.js
@@ -1,6 +1,6 @@
 var jq = $.noConflict();
 jq(document).ready(function() {
-    var API_URL = "https://fd25hjufdwhtrnbmnuglrsvigq0ayuef.lambda-url.eu-west-1.on.aws/"
+    var API_URL = "https://fd25hjufdwhtrnbmnuglrsvigq0ayuef.lambda-url.eu-west-1.on.aws"
     $.getJSON(API_URL, function(data) {
       console.log(data);
     //   $("#sample-data").html(data['ctRoot'][0]['description']);
